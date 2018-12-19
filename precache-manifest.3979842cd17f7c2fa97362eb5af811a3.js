55a760068b71fb213f64087abd11d81d968a7454
Kristy Chen <kristychen118@gmail.com>
2018-12-19 02:49:28 -0500

    Updates

---
+++ precache-manifest.3979842cd17f7c2fa97362eb5af811a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a738aac8b7a991bba87e9b5c8b65c0d",
+    "url": "/vimeo-code-challenge/static/media/hexigon.9a738aac.svg"
+  },
+  {
+    "revision": "634e15492ef46c32b7d1708a41320a34",
+    "url": "/vimeo-code-challenge/static/media/coco.634e1549.jpg"
+  },
+  {
+    "revision": "a405cec134b39c0b5cbd67997d08a062",
+    "url": "/vimeo-code-challenge/static/media/WreckItRalph2.a405cec1.jpg"
+  },
+  {
+    "revision": "c5c30573d496d792117870583bf8ab64",
+    "url": "/vimeo-code-challenge/static/media/InfinityWar.c5c30573.jpg"
+  },
+  {
+    "revision": "cc52ae57200b99973cd1",
+    "url": "/vimeo-code-challenge/static/js/runtime~main.cc52ae57.js"
+  },
+  {
+    "revision": "d167bd3b2f1bfd146c6f",
+    "url": "/vimeo-code-challenge/static/js/main.d167bd3b.chunk.js"
+  },
+  {
+    "revision": "d0fa5940a5d81eddbd68",
+    "url": "/vimeo-code-challenge/static/js/1.d0fa5940.chunk.js"
+  },
+  {
+    "revision": "d167bd3b2f1bfd146c6f",
+    "url": "/vimeo-code-challenge/static/css/main.11b8a9f7.chunk.css"
+  },
+  {
+    "revision": "d0fa5940a5d81eddbd68",
+    "url": "/vimeo-code-challenge/static/css/1.69ce72bb.chunk.css"
+  },
+  {
+    "revision": "5f60c2c735a378befe7d69930b5a8f06",
+    "url": "/vimeo-code-challenge/index.html"
+  }
+];
